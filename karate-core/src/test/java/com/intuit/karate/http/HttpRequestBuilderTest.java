d9e9ad8f8b7421e4b51de773aed0d13ffac17015
Melissa Mazura <mazura@kth.se>
2024-02-20 13:17:17 +0100

    test: add test cases for the coverage

--- karate-core/src/test/java/com/intuit/karate/http/HttpRequestBuilderTest.java
+++ karate-core/src/test/java/com/intuit/karate/http/HttpRequestBuilderTest.java
@@ -1,7 +1,16 @@
 package com.intuit.karate.http;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import org.graalvm.polyglot.Value;
+
+
 
 import com.intuit.karate.core.ScenarioEngine;
 import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
@@ -17,10 +26,45 @@ class HttpRequestBuilderTest {
 
     HttpRequestBuilder http;
 
+    @BeforeAll
+    public static void setUp() {
+        Coverage.visited = new boolean[16];
+    }
+
+    @AfterAll
+    public static void showBranchCoverage() throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter("coverage_melissa.txt"));
+        int totalBranchesCovered = 0;
+        for (int i = 0; i < Coverage.visited.length; i++) {
+            bufferedWriter.write("Branch ID: " + i + " Covered: " + Coverage.visited[i] + "\n");
+            if (Coverage.visited[i]) {
+                totalBranchesCovered++;
+            }
+        }
+        bufferedWriter.write("branches covered %: " +  ((float) totalBranchesCovered/ (float) Coverage.visited.length) + "\n");
+        bufferedWriter.close();
+    }
+
+
+
     @BeforeEach
     void beforeEach() {
         ScenarioEngine se = ScenarioEngine.forTempUse(HttpClientFactory.DEFAULT);
         http = new HttpRequestBuilder(HttpClientFactory.DEFAULT.create(se));
+    }
+    @Test
+    void testGetMemberURL() {
+        HttpClient client = null;
+        HttpRequestBuilder newFile = new HttpRequestBuilder(client);
+        assertTrue(newFile.getMember("method") != null);
+    }
+
+    @Test
+    void testGetMemberDefaultCase() {
+        HttpClient client = null;
+        HttpRequestBuilder newFile = new HttpRequestBuilder(client);
+        Object hello = newFile.getMember("0");
+        assertNull(hello);
     }
 
     @Test
